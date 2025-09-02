80fab635c47ac2b8c4557deb90d3ecba17ed9c1d
Mohamed Shahat <mshahat@computer.org>
2025-09-02 20:29:25 +0200

    Use ready embeddings during streamlit init

--- streamlit.py
+++ streamlit.py
@@ -5,6 +5,8 @@ Here's our first attempt at using data to create a table:
 
 import streamlit as st
 import pandas as pd
+import ast
+
 from main import generate_embeddings
 
 def init_data():
@@ -13,10 +15,17 @@ def init_data():
     print(styles_df.head())
     print("Opened dataset successfully. Dataset has {} items of clothing.".format(len(styles_df)))
 
-    generate_embeddings(styles_df, 'productDisplayName')
-    print("Writing embeddings to file ...")
-    styles_df.to_csv('data/sample_clothes/sample_styles_with_embeddings.csv', index=False)
-    print("Embeddings successfully stored in sample_styles_with_embeddings.csv")
+    # MOHAMED - Not Generating own embeddings for demo
+    # generate_embeddings(styles_df, 'productDisplayName')
+    # print("Writing embeddings to file ...")
+    # styles_df.to_csv('data/sample_clothes/sample_styles_with_embeddings.csv', index=False)
+    # print("Embeddings successfully stored in sample_styles_with_embeddings.csv")
+
+    # MOHAMED - Use already generated embeddings for demo
+    styles_df = pd.read_csv('data/sample_clothes/sample_styles_with_embeddings.csv', on_bad_lines='skip')
+
+    # Convert the 'embeddings' column from string representations of lists to actual lists of floats
+    styles_df['embeddings'] = styles_df['embeddings'].apply(lambda x: ast.literal_eval(x))
 
     print(styles_df.head())
     print("Opened dataset successfully. Dataset has {} items of clothing along with their embeddings.".format(len(styles_df)))
